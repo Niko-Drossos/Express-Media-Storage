a9ce9d0fa8ed04bd9c21184f6ca87b8795ec6e03
Niko-Drossos <8sHe682GSYhaas@protonmail.com>
2024-04-24 00:14:19 -0700

    Added error handling

--- src/models/middleware/authenticateUserJWT.js
+++ src/models/middleware/authenticateUserJWT.js
@@ -2,20 +2,30 @@ const decryptJWT = require("../../helpers/decryptJWT");
 
 // Decode JWT middleware to extract the user ID
 module.exports = authenticateUserJWT = (req, res, next) => {
-  const token = req.headers.authorization;
+  try {
 
-  if (!token) {
-    return res.status(401).json({
+    const token = req.headers.authorization;
+    
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "No token provided, please log in",
+      });
+    }
+
+    const decodedToken = decryptJWT(token);
+    
+    req.userId = decodedToken.payload.userId || null
+    req.username = decodedToken.payload.username || null
+    req.email = decodedToken.payload.email || null
+    
+    next()
+  } catch (error) {
+    res.status(403).json({
       success: false,
-      message: "No token provided, please log in",
-    });
+      message: "Failed to validate user",
+      errorMessage: error.message,
+      error
+    })
   }
-
-  const decodedToken = decryptJWT(token);
-
-  req.userId = decodedToken.payload.userId || null
-  req.username = decodedToken.payload.username || null
-  req.email = decodedToken.payload.email || null
-
-  next()
 }
