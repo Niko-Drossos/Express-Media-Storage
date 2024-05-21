48ede02541877b047fafb4dbdc4ebf59bd6ddfcb
Niko-Drossos <8sHe682GSYhaas@protonmail.com>
2024-05-21 09:30:51 -0700

    Added multer middleware to post routes for FormData parsing

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -4,17 +4,18 @@ const router = express.Router()
 /* ------------------------------- Controllers ------------------------------ */
 const postController = require("../controllers/postController")
 /* ------------------------------- Middleware ------------------------------- */
+const { upload } = require("../helpers/gridFsMethods")
 const authenticateUserJWT = require("../models/middleware/authenticateUserJWT")
 router.all("/*", authenticateUserJWT)
 /* -------------------------------------------------------------------------- */
 
 router.get("/:postId", postController.getPost)
 
-router.post("/create", postController.createPost)
+router.post("/create", upload.none(), postController.createPost)
 
-router.post("/journal/add/:postId", postController.addJournal)
+router.post("/journal/add/:postId", upload.none(), postController.addJournal)
 
-router.put("/edit/:postId", postController.editPost)
+router.put("/edit/:postId", upload.none(), postController.editPost)
 
 router.delete("/delete/:postId", postController.deletePost)
 
